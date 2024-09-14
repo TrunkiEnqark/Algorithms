592b796bfb861f2ed2e795704b6c8a5b02074ca2
Trunki Enqark <trunki.enqark@gmail.com>
2024-09-14 21:41:19 +0700

    add new data structure: Singly Linked List using rust

--- DATA STRUCTURES/Linked List/singly.rs
+++ DATA STRUCTURES/Linked List/singly.rs
@@ -1,18 +1,39 @@
-struct Node<T> {
+use std::fmt;
+
+pub struct Node<T> {
     data: T,
     next: Option<Box<Node<T>>>,
 }
 
-struct SinglyLinkedList<T> {
+impl<T> Node<T> {
+    pub fn new(data: T) -> Box<Self> {
+        Box::new(Node {
+            data: data,
+            next: None,
+        })
+    }
+}
+
+pub struct SinglyLinkedList<T> {
     head: Option<Box<Node<T>>>,
 }
 
 impl<T> SinglyLinkedList<T> {
-    fn new() -> Self {
+    pub fn new() -> Self {
         SinglyLinkedList { head: None }
     }
 
-    fn push_front(&mut self, data: T) {
+    pub fn len(&self) -> usize {
+        let mut length: usize = 0;
+        let mut current_node = &self.head;
+        while let Some(node) = current_node {
+            length += 1;
+            current_node = &node.next;
+        }
+        length
+    }
+
+    pub fn push_front(&mut self, data: T) {
         let new_node = Box::new(Node {
             data,
             next: self.head.take(),
@@ -20,12 +41,42 @@ impl<T> SinglyLinkedList<T> {
         self.head = Some(new_node);
     }
 
-    fn pop_front(&mut self, data: T) -> Option<&T> {
+    pub fn pop_front(&mut self) -> Option<T> {
         self.head.take().map(|node| {
-            self.head = node.next,
+            self.head = node.next;
             node.data
         })
     }
 
-    
+    pub fn push_back(&mut self, data: T) {
+        let new_node = Node::new(data);
+        match self.head.as_mut() {
+            None => self.head = Some(new_node),
+            Some(mut current) => {
+                while current.next.is_some() {
+                    current = current.next.as_mut().unwrap();
+                }
+                current.next = Some(new_node);
+            }
+        }
+    }
+
+    pub fn pop_back(&mut self) {
+        
+    }
+}
+
+impl<T: fmt::Display> fmt::Display for SinglyLinkedList<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[")?;
+        let mut current = &self.head;
+        while let Some(node) = current {
+            write!(f, "{}", node.data)?;
+            current = &node.next;
+            if current.is_some() {
+                write!(f, ", ")?;
+            }
+        }
+        write!(f, "]")
+    }
 }
