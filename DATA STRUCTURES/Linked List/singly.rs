d45fbe780b73b429b6ceda5d92098366f301956e
Trunki Enqark <trunki.enqark@gmail.com>
2024-09-14 22:15:50 +0700

    add some features for singly.rs

--- DATA STRUCTURES/Linked List/singly.rs
+++ DATA STRUCTURES/Linked List/singly.rs
@@ -33,6 +33,10 @@ impl<T> SinglyLinkedList<T> {
         length
     }
 
+    pub fn is_empty(&self) -> bool {
+        self.head.is_none()
+    }
+
     pub fn push_front(&mut self, data: T) {
         let new_node = Box::new(Node {
             data,
@@ -61,8 +65,43 @@ impl<T> SinglyLinkedList<T> {
         }
     }
 
-    pub fn pop_back(&mut self) {
-        
+    pub fn pop_back(&mut self) -> Option<T> {
+        match self.head.as_mut() {
+            None => None,
+            Some(mut current) if current.next.is_none() => {
+                self.head.take().map(|node| node.data)
+            }
+            Some(mut current) => {
+                while current.next.as_ref().unwrap().next.is_some() {
+                    current = current.next.as_mut().unwrap();
+                }
+                current.next.take().map(|node| node.data)
+            }
+        }
+    }
+
+    pub fn peek_front(&mut self) -> Option<&T> { // return immutable reference
+        self.head.as_ref().map(|node| &node.data)
+    }
+
+    pub fn peek_front_mut(&mut self) -> Option<&mut T> { // return mutable reference
+        self.head.as_mut().map(|node| &mut node.data)
+    }
+
+    pub fn peek_back(&self) -> Option<&T> { // return immutable reference
+        let mut current = self.head.as_ref()?;
+        while let Some(next) = current.next.as_ref() {
+            current = next;
+        }
+        Some(&current.data)
+    }
+
+    pub fn peek_back_mut(&mut self) -> Option<&mut T> { // return mutable reference
+        let mut current = self.head.as_mut()?;
+        while let Some(next) = current.next.as_mut() {
+            current = next;
+        }
+        Some(&mut current.data)
     }
 }
 
